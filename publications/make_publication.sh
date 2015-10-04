3f6a12a69130d62123bcdfde28d65cd6bb26f6f3
deploy.sh <muranushi@gmail.com>
2015-10-04 14:04:03 +0900

    using CJK

--- publications/make_publication.sh
+++ publications/make_publication.sh
@@ -1,9 +1,9 @@
 mkdir -p dist
-cp publications.tex *.bib *.bst *.sty dist/
+cp publications.tex *.bib *.bst *.sty *.cfg dist/
 cd dist
-htlatex publications.tex '' ''
+htlatex publications.tex  "jppp,xhtml,charset=utf-8" " -cunihtf -utf8"
 for auxfile in $( ls bu*.aux ); do
     echo bibtex $auxfile
     bibtex $auxfile
 done
-htlatex publications.tex '' ''
+htlatex publications.tex  "jppp,xhtml,charset=utf-8" " -cunihtf -utf8"
