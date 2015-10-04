306186f179ce324368dda8d588128cefccfd31a5
deploy.sh <muranushi@gmail.com>
2015-10-04 15:18:57 +0900

    most of character available

--- publications/make_publication.sh
+++ publications/make_publication.sh
@@ -1,9 +1,9 @@
 mkdir -p dist
 cp publications.tex *.bib *.bst *.sty *.cfg dist/
 cd dist
-htlatex publications.tex  "jp,xhtml,charset=utf-8" " -cunihtf -utf8"
+htlatex publications.tex  "xhtml,charset=utf-8" " -cunihtf -utf8"
 for auxfile in $( ls bu*.aux ); do
     echo bibtex $auxfile
     bibtex $auxfile
 done
-htlatex publications.tex  "jp,xhtml,charset=utf-8" " -cunihtf -utf8"
+htlatex publications.tex  "xhtml,charset=utf-8" " -cunihtf -utf8"
