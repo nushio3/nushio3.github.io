81deaf498ea87496f8996a246c81ebd21c4913a5
deploy.sh <muranushi@gmail.com>
2015-10-04 12:50:08 +0900

    add books

--- publications/make_publication.sh
+++ publications/make_publication.sh
@@ -1,9 +1,9 @@
 mkdir -p dist
-cp publications.tex publications.bib *.bst *.sty dist/
+cp publications.tex *.bib *.bst *.sty dist/
 cd dist
 htlatex publications.tex '' ''
 for auxfile in $( ls bu*.aux ); do
     echo bibtex $auxfile
     bibtex $auxfile
 done
-#htlatex publications.tex '' ''
+htlatex publications.tex '' ''
